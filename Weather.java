5a8c302043bacb3afae9283a6eac16f36c478d3c
envokeME <45800737+envokeME@users.noreply.github.com>
2019-04-04 16:40:30 -0700

    Java

---
+++ Weather.java
@@ -0,0 +1,8 @@
+package com.grossmont.ws;
+
+public class Weather {
+
+
+    Weather_main main = new Weather_main();
+    String name;
+}
---
+++ Weather_main.java
@@ -0,0 +1,6 @@
+package com.grossmont.ws;
+
+public class Weather_main {
+
+   public float temp, pressure, humidity, temp_min, temp_max;
+}
